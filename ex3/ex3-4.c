8430c2465334c27a5b0a4317a051ade1bd2bc641
tetsu5555 <tetete1118@gmail.com>
2021-04-02 09:14:52 +0900

    ex3

---
+++ ex3/ex3-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+  int i;
+
+  i = 0;
+  for (;;) {
+    printf("%d ", i);
+    i++;
+  }
+  return 0;
+}
---
+++ ex3/ex3-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, j;
+  i = 1;
+
+  while (i < 10) {
+    j = 1;
+    while (j < 10) {
+      printf("%02d ", i * j);
+      j++;
+    }
+    printf("\n");
+    i++;
+  }
+  
+  
+  return 0;
+}
